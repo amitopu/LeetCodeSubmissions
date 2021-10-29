7de834cd0197b8a2e32469e8c4435e3187d78486
rupok <nurealamsiddiky@gmail.com>
2021-10-29 10:20:36 +0600

    Solution to "Ransom Note" is added

---
+++ RansomNote/ransomNote.py
@@ -0,0 +1,54 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomDict = {}
+        magazineDict = {}
+        
+        # collect ransomNote char frequency
+        for i in ransomNote:
+            if ransomDict.get(i, -1) == -1:
+                ransomDict[i] = 1
+            else:
+                ransomDict[i] += 1
+        
+        # collect magazine chat frequency
+        for j in magazine:
+            if magazineDict.get(j, -1) == -1:
+                magazineDict[j] = 1
+            else:
+                magazineDict[j] += 1
+        
+        # check if all char in ransomNote can be covered by magazine
+        count = 0
+        for elem in ransomDict.keys():
+            if magazineDict.get(elem, -1) >= ransomDict[elem]:
+                count += 1
+        
+        if count == len(ransomDict.keys()):
+            return True
+        else:
+            return False
+
+
+
+# different solution reduces memory requirement
+
+# class Solution:
+#     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+#         magazineDict = {}
+        
+#         for j in magazine:
+#             if magazineDict.get(j, -1) == -1:
+#                 magazineDict[j] = 1
+#             else:
+#                 magazineDict[j] += 1
+        
+#         count = 0
+#         for i in ransomNote:
+#             if magazineDict.get(i, -1) > 0:
+#                 magazineDict[i] -= 1
+#                 count += 1
+                
+#         if count == len(ransomNote):
+#             return True
+#         else:
+#             return False
